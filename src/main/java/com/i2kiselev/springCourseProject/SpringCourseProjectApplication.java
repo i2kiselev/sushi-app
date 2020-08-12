7c5a95559b937e4f58cb986218cbd7dfd2ecf9ab
i2kiselev <griggs1299@gmail.com>
2020-08-12 18:08:34 +0800

    Added .gitignore file

--- src/main/java/com/i2kiselev/springCourseProject/SpringCourseProjectApplication.java
+++ src/main/java/com/i2kiselev/springCourseProject/SpringCourseProjectApplication.java
@@ -1,14 +1,7 @@
 package com.i2kiselev.springCourseProject;
 
-import com.i2kiselev.springCourseProject.model.AbstractProduct;
-import com.i2kiselev.springCourseProject.model.Product;
-import com.i2kiselev.springCourseProject.model.Roll;
-import com.i2kiselev.springCourseProject.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import sun.tools.jar.CommandLine;
 
 @SpringBootApplication
 public class SpringCourseProjectApplication {
