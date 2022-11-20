ca7b70ddab15bb76fa77b2939d18d70d34fa7a48
i2kiselev <griggs1299@gmail.com>
2022-11-20 19:22:29 +0300

    Fixes to order repo tests

--- src/test/java/com/i2kiselev/springCourseProject/repository/OrderRepositoryTest.java
+++ src/test/java/com/i2kiselev/springCourseProject/repository/OrderRepositoryTest.java
@@ -2,7 +2,6 @@ package com.i2kiselev.springCourseProject.repository;
 
 import com.i2kiselev.springCourseProject.model.Order;
 import com.i2kiselev.springCourseProject.model.User;
-import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -23,46 +23,78 @@ class OrderRepositoryTest {
     UserRepository userRepository;
 
     @Test
-    void findById(){
-        Order order = new Order();
-        orderRepository.save(order);
-        Order orderTest = orderRepository.findById(order.getId()).get();
-        assertThat(orderTest.getId()).isEqualTo(order.getId());
-    }
-
-    @Test
-    void emptyFindAllByStatusIsNot(){
+    void findAllByStatusIsNot_emptyResult(){
+        //setup
         Order order = new Order();
         order.setStatus(Order.Status.ACCEPTED);
         orderRepository.save(order);
+
+        //act
         Page<Order> orderPage  = orderRepository.findAllByStatusIsNot(Order.Status.ACCEPTED, Pageable.unpaged());
+
+        //verify
         assertThat(orderPage.getTotalElements()).isEqualTo(0);
     }
 
     @Test
-    void notEmptyFindAllByStatusIsNot(){
+    void findAllByStatusIsNot_positive(){
+        //setup
         Order order = new Order();
         order.setStatus(Order.Status.ACCEPTED);
         orderRepository.save(order);
+
+        //act
         Page<Order> orderPage  = orderRepository.findAllByStatusIsNot(Order.Status.FINISHED, Pageable.unpaged());
-        assertThat(orderPage.getTotalElements()).isEqualTo(1);
+
+        //verify
+        assertEquals(1, orderPage.getTotalElements());
     }
 
     @Test
-    void notEmptyFindAllByUser(){
+    void findAllByStatusIsNot_onlyNegatedType(){
+        //setup
+        Order order = new Order();
+        order.setStatus(Order.Status.FINISHED);
+        Order order2 = new Order();
+        order2.setStatus(Order.Status.FINISHED);
+        orderRepository.save(order);
+        orderRepository.save(order2);
+
+        //act
+        Page<Order> orderPage  = orderRepository.findAllByStatusIsNot(Order.Status.FINISHED, Pageable.unpaged());
+
+        //verify
+        assertTrue(orderPage.toList().isEmpty());
+    }
+
+    @Test
+    void findAllByUser_positive(){
+        //setup
         User user = new User();
         user.setUsername("user");
         userRepository.save(user);
-        Order order = new Order();
-        order.setUser(user);
-        order.setStatus(Order.Status.ACCEPTED);
-        orderRepository.save(order);
+
+        Order userOrder = new Order();
+        userOrder.setUser(user);
+        userOrder.setStatus(Order.Status.ACCEPTED);
+
+        Order orderWithoutUser = new Order();
+
+        orderRepository.save(userOrder);
+        orderRepository.save(orderWithoutUser);
+
+        //act
         Page<Order> orderPage = orderRepository.findAllByUser(user,Pageable.unpaged());
-        assertThat(orderPage.getTotalElements()).isEqualTo(1);
+
+        //verify
+        assertEquals(1, orderPage.getTotalElements());
+        Order result = orderPage.toList().get(0);
+        assertSame(userOrder, result);
     }
 
     @Test
-    void emptyFindAllByUser(){
+    void findAllByUser_emptyResult(){
+        //setup
         User user = new User();
         user.setUsername("user");
         userRepository.save(user);
@@ -70,7 +102,11 @@ class OrderRepositoryTest {
         orderRepository.save(order);
         User testUser = new User( "user2");
         userRepository.save(testUser);
+
+        //act
         Page<Order> orderPage = orderRepository.findAllByUser(testUser,Pageable.unpaged());
-        assertThat(orderPage.getTotalElements()).isEqualTo(0);
+
+        //verify
+        assertTrue(orderPage.toList().isEmpty());
     }
 }
